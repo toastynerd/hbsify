10c2acc88793c870bcc6f40cc0bdef6eac27db2b
toastynerd <tylermorgan86@gmail.com>
2014-08-22 12:53:32 -0700

    fixing typos

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ ! -h node_modules/hbsfy ]; then
-    rm -rf node_modules/hbsfy
-    ln -fs .. node_modules/hbsfy
+if [ ! -h node_modules/hbsify ]; then
+    rm -rf node_modules/hbsify
+    ln -fs .. node_modules/hbsify
 fi
 
 ./node_modules/grunt-cli/bin/grunt test
