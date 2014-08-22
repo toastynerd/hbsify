dffd796a3668c954be8a174a268140aad07e6235
toastynerd <tylermorgan86@gmail.com>
2014-08-22 10:14:47 -0700

    version 1.0

---
+++ index.js
@@ -0,0 +1,25 @@
+var Handlebars = require('handlebars');
+var through = require('through');
+
+var hbsify = function(file) {
+  var exts = ['hbs', 'handlebars'];
+  var data = '';  
+
+  if (exts.indexOf(file.split('.').pop()) < 0) return through();
+
+  return through(write, end);
+
+  function write(buf) {
+    data += buf;
+  } 
+
+  function end() {
+    var precomp = Handlebars.precompile(data);
+    var template = 'var Handlebars = require("handlebars/runtime")["default"];\n';
+    template += 'module.exports = Handlebars.template(' + precomp.toString() + ');\n';
+    this.queue(template);
+    this.queue(null); 
+  }
+};
+
+module.exports = hbsify;
